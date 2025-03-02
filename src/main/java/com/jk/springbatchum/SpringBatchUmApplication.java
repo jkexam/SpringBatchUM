2876fe5051adb7f55cc2886f266a9c6584459a60
JKOOGI-HP\jkoog <jkoogi@naver.com>
2025-03-03 00:49:08 +0900

    * 스프링 배치프로젝트 생성

---
+++ src/main/java/com/jk/springbatchum/SpringBatchUmApplication.java
@@ -0,0 +1,13 @@
+package com.jk.springbatchum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchUmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchUmApplication.class, args);
+    }
+
+}
