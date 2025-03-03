78c8a99bf2d11bd28ef1ff711e3fe3eba19420a0
JKOOGI-HP\jkoog <jkoogi@naver.com>
2025-03-03 16:49:48 +0900

    * 코드정리

--- src/main/java/com/jk/springbatchum/config/DataDBConfig.java
+++ src/main/java/com/jk/springbatchum/config/DataDBConfig.java
@@ -1,19 +1,16 @@
 package com.jk.springbatchum.config;
 
-import lombok.Builder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
-import java.beans.ConstructorProperties;
 import java.util.HashMap;
 
 @Configuration
--- src/main/java/com/jk/springbatchum/config/MetaDBConfig.java
+++ src/main/java/com/jk/springbatchum/config/MetaDBConfig.java
@@ -5,12 +5,10 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
-import java.beans.ConstructorProperties;
 
 @Configuration
 public class MetaDBConfig {
