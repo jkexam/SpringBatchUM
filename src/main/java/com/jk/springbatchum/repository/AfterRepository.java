2660fb482a50422f8ec416b8bba08e2ee9a164ef
JKOOGI-HP\jkoog <jkoogi@naver.com>
2025-03-03 17:37:13 +0900

    * 6.1 첫번째 배치를 위한 엔티티 소스 추가  - Entity, Repository

---
+++ src/main/java/com/jk/springbatchum/repository/AfterRepository.java
@@ -0,0 +1,7 @@
+package com.jk.springbatchum.repository;
+
+import com.jk.springbatchum.entity.AfterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AfterRepository extends JpaRepository<AfterEntity, Long> {
+}
---
+++ src/main/java/com/jk/springbatchum/repository/BeforeRepository.java
@@ -0,0 +1,8 @@
+package com.jk.springbatchum.repository;
+
+import com.jk.springbatchum.entity.BeforeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BeforeRepository extends JpaRepository<BeforeEntity,Long> {
+
+}
