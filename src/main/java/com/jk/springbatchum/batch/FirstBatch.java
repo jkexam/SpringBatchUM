473c65ad3c30903ef76c15cb507f3df43dca1b1e
JKOOGI-HP\jkoog <jkoogi@naver.com>
2025-03-03 19:03:08 +0900

    * 6.3 첫번째 배치 step 메서드 추가

--- src/main/java/com/jk/springbatchum/batch/FirstBatch.java
+++ src/main/java/com/jk/springbatchum/batch/FirstBatch.java
@@ -1,10 +1,14 @@
 package com.jk.springbatchum.batch;
 
+import com.jk.springbatchum.entity.AfterEntity;
+import com.jk.springbatchum.entity.BeforeEntity;
 import com.jk.springbatchum.repository.AfterRepository;
 import com.jk.springbatchum.repository.BeforeRepository;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -33,6 +37,19 @@ public class FirstBatch {
         return new JobBuilder("firstJob", jobRepository)
                 .start(firstStep())
                 .build();
+    }
+
+    @Bean
+    public Step firstStep() {
+        System.out.println("First Step");
+
+        return new StepBuilder("firstStep", jobRepository)
+                .<BeforeEntity, AfterEntity> chunk(10, platformTransactionManager)
+//                .reader("배치대상정보 읽기 메서드")
+//                .processor("배치 처리 메서드")
+//                .writer("배치대상정보 쓰기 메서드")
+                .build();
+
     }
 
 }
